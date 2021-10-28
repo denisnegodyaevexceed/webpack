c9ffed2f062abdd238e6f597a469f151183d0749
denisnegodyaevexceed <dnegodyaev.exceedteam@gmail.com>
2021-10-28 20:26:40 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path")
+
+module.exports = (env) => {
+    const modules = {
+        js: {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: "ts-loader",
+                },
+            ],
+        },
+        stylus: {
+            test: /\.styl$/,
+            use: [
+                {
+                    loader: "style-loader",
+                },
+                {
+                    loader: "css-loader",
+                },
+                {
+                    loader: "stylus-loader",
+                    options: {
+                        import: [ 
+                            path.resolve(__dirname, 'src/Common/Styles/variables.styl'),
+                        ],
+                    }
+                },
+            ],
+        },
+    }
+
+    if (env === 'production') {
+        modules.stylus.use.splice(2, 0, { loader: "postcss-loader" })
+    }
+
+    const resolve = {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        alias: { 
+            App: path.resolve(__dirname, 'src/App/'),
+            Pages: path.resolve(__dirname, 'src/Pages/'),
+        },
+    }
+
+    return {
+        modules,
+        resolve,
+    }
+}
